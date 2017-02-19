fcd66c8b43f86bf0e4d7575001b5905884b1c560
Andrian Krysak <andriankrysak@gmail.com>
2017-02-19 11:59:20 +0200

    enable map scroll on click only

--- js/script.js
+++ js/script.js
@@ -79,3 +79,19 @@ $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
 
 ////////////////////////SCROLL ANIMATION END ///////////////////////
+
+$(document).ready(function () {
+
+        // you want to enable the pointer events only on click;
+
+        $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#canvas1').on('click', function () {
+            $('#map_canvas1').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+        // you want to disable pointer events when the mouse leave the canvas area;
+
+        $("#map_canvas1").mouseleave(function () {
+            $('#map_canvas1').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+        });
+    });
